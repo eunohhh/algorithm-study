c57bdf394e5651fba30c1956f2eea77ffa2233ae
Eun Oh <bdohhhhh@gmail.com>
2024-10-27 12:15:14 +0900

    [add]: 10

---
+++ section01/10.js
@@ -0,0 +1,20 @@
+// 한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개
+// 존재하는지 알아내는 프로그램을 작성하세요.
+// 문자열의 길이는 100을 넘지 않습니다.
+function solution({ s, word }) {
+  const filtered = [...word].filter((character) => character === s);
+  return filtered.length;
+}
+
+const inputs = [{ s: 'R', word: 'COMPUTERPROGRAMMING' }];
+const expected = [3];
+
+function test(inputs, expected) {
+  const solved = inputs.map((input) => solution(input));
+  if (JSON.stringify(solved) === JSON.stringify(expected)) {
+    console.log(`정답입니다. 출력 ${solved}`);
+  } else {
+    console.log(`오답입니다. 예상 ${expected}, 출력 ${solved}`);
+  }
+}
+test(inputs, expected);
