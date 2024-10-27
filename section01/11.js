1aebb8b92f5951af8f824249454a352e96ac45e5
Eun Oh <bdohhhhh@gmail.com>
2024-10-27 12:23:01 +0900

    [add]: 11

---
+++ section01/11.js
@@ -0,0 +1,19 @@
+// 한 개의 문자열을 입력받아 해당 문자열에 알파벳 대문자가 몇 개 있는지 알아내는 프로그램
+// 을 작성하세요.
+function solution(word) {
+  const filtered = [...word].filter((character) => character === character.toUpperCase());
+  return filtered.length;
+}
+
+const inputs = ['KoreaTimeGood'];
+const expected = [3];
+
+function test(inputs, expected) {
+  const solved = inputs.map((input) => solution(input));
+  if (JSON.stringify(solved) === JSON.stringify(expected)) {
+    console.log(`정답입니다. 출력 ${solved}`);
+  } else {
+    console.log(`오답입니다. 예상 ${expected}, 출력 ${solved}`);
+  }
+}
+test(inputs, expected);
