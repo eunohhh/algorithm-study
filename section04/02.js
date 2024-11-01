0061806dbba36f507b3bcd445747809ed7a690d3
Eun Oh <bdohhhhh@gmail.com>
2024-11-01 22:18:51 +0900

    [add]: s4 02

---
+++ section04/02.js
@@ -0,0 +1,30 @@
+// 뒤집은 소수
+// N개의 자연수가 입력되면 각 자연수를 뒤집은 후 그 뒤집은 수가 소수이면 그 소수를 출력하
+// 는 프로그램을 작성하세요. 예를 들어 32를 뒤집으면 23이고, 23은 소수이다. 그러면 23을 출
+// 력한다. 단 910를 뒤집으면 19로 숫자화 해야 한다. 첫 자리부터의 연속된 0은 무시한다.
+function solution(arr) {
+  const isPrime = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+
+  return arr
+    .map((num) => Number(num.toString().split('').reverse().join('')))
+    .filter((num) => isPrime(num));
+}
+
+const inputs = [[32, 55, 62, 20, 250, 370, 200, 30, 100]];
+const expected = [[23, 2, 73, 2, 3]];
+
+function test(inputs, expected) {
+  const solved = inputs.map((input) => solution(input));
+  if (JSON.stringify(solved) === JSON.stringify(expected)) {
+    console.log(`정답입니다. 출력 ${solved}`);
+  } else {
+    console.log(`오답입니다. 예상 ${expected}, 출력 ${solved}`);
+  }
+}
+test(inputs, expected);
